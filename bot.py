9cc31abfa684b4f2415681de943c683f287731ce
Hahqhqjqjqqjq <166511888+Hahqhqjqjqqjq@users.noreply.github.com>
2024-05-08 23:00:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,167 +1,42 @@
-import telebot
-import json
-import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
 
-API_KEY = "7122754136:AAE-8mr83R7_O8w0R5bJIxEsYIKE3VlD1FU"
-bot = telebot.TeleBot(API_KEY)
+import asyncio
+import datetime
+import telegram
+import pytz
 
-user_data = {}
+# استبدال "TOKEN" بتوكن البوت الخاص بك
+bot = telegram.Bot(token="7122754136:AAE-8mr83R7_O8w0R5bJIxEsYIKE3VlD1FU")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    chat_id = message.chat.id
-    user_data[chat_id] = {"mode": None, "mails": []}
+# استبدال "CHANNEL_ID" بمعرف القناة الذي تريد تحديث وصفها
+channel_id = "@UU_Le2"
 
-    bot.send_message(chat_id, """
-    اهلا بك في بوت ارسال الرسائل الإلكترونية التلقائي
-    """)
-    show_keyboard(chat_id)
+# إضافة النص الثابت هنا
+description = """• كل مايخص المجال موجود هنا 
+شروحات لبرامج ارقام وهميه
+جميع حلول الواتساب
+وثغرات فك وحول لجميع منصات التواصل الاجتماعي"""
 
-def show_keyboard(chat_id):
-    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row("أضف بريدات", "حذف البريدات")
-    markup.row("عرض البريدات المضافة", "ارسال رسالة")
-    bot.send_message(chat_id, "اختر الإجراء الذي ترغب في تنفيذه:", reply_markup=markup)
+async def update_channel_description():
+    while True:
+        try:
+            # الحصول على التاريخ والوقت الحالي بتوقيت مصر
+            tz = pytz.timezone( Asia/Yemen )
+            now = datetime.datetime.now(tz)
+            current_date = now.strftime("%d/%m/%Y")
+            current_time = now.strftime("%H:%M:%S")
 
-@bot.message_handler(func=lambda message: message.text == "أضف بريدات")
-def add_email_step1(message):
-    chat_id = message.chat.id
-    user_data[chat_id]["mode"] = "addmail"
-    bot.send_message(chat_id, "ارسل الآن البريد الإلكتروني الذي تريد إضافته:")
+            # تحديث وصف القناة بالتاريخ والوقت الحالي والنص الثابت
+            description_with_time = f"{description}\n\n🗓| التاريخ :  {current_date}\n⏱| الوقت : {current_time}"
+            await bot.setChatDescription(chat_id=channel_id, description=description_with_time)
 
-@bot.message_handler(func=lambda message: user_data[message.chat.id]["mode"] == "addmail")
-def add_email_step2(message):
-    chat_id = message.chat.id
-    email = message.text
+        except telegram.TelegramError as e:
+            print(f"An error occurred: {e}")
 
-    if email not in user_data[chat_id]["mails"]:
-        user_data[chat_id]["mails"].append(email)
-        bot.send_message(chat_id, "تم الحفظ والإضافة ✅")
-    else:
-        bot.send_message(chat_id, "البريد الإلكتروني موجود بالفعل.")
+        # انتظر لمدة 60 ثانية قبل تحديث وصف القناة مرة أخرى
+        await asyncio.sleep(60)
 
-    user_data[chat_id]["mode"] = None
-    show_keyboard(chat_id)
-
-@bot.message_handler(func=lambda message: message.text == "حذف البريدات")
-def delete_emails(message):
-    chat_id = message.chat.id
-    user_data[chat_id]["mode"] = "delmail"
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    for i, email in enumerate(user_data[chat_id]["mails"]):
-        callback_data = f"delt_{i}"
-        keyboard.add(telebot.types.InlineKeyboardButton(email, callback_data=callback_data))
-    bot.send_message(chat_id, "اختر البريد الإلكتروني الذي تريد حذفه:", reply_markup=keyboard)
-
-@bot.callback_query_handler(func=lambda call: call.data.startswith("delt_"))
-def delete_email_callback(call):
-    chat_id = call.message.chat.id
-    email_index = int(call.data.split("_")[1])
-
-    if email_index < len(user_data[chat_id]["mails"]):
-        deleted_email = user_data[chat_id]["mails"].pop(email_index)
-        bot.send_message(chat_id, f"تم حذف البريد الإلكتروني: {deleted_email} ✅")
-
-    user_data[chat_id]["mode"] = None
-    show_keyboard(chat_id)
-
-@bot.message_handler(func=lambda message: message.text == "عرض البريدات المضافة")
-def view_emails(message):
-    chat_id = message.chat.id
-    if not user_data[chat_id]["mails"]:
-        bot.send_message(chat_id, "لم تقم بإضافة أي بريد إلكتروني بعد.")
-    else:
-        email_list = "\n".join(user_data[chat_id]["mails"])
-        bot.send_message(chat_id, f"البريدات المضافة:\n{email_list}")
-    show_keyboard(chat_id)
-
-@bot.message_handler(func=lambda message: message.text == "ارسال رسالة")
-def send_email_step1(message):
-    chat_id = message.chat.id
-    user_data[chat_id]["mode"] = "sendms"
-    bot.send_message(chat_id, "ارسل البريد الإلكتروني الذي تريد إرسال الرسالة إليه:")
-
-@bot.message_handler(func=lambda message: user_data[message.chat.id]["mode"] == "sendms")
-def send_email_step2(message):
-    chat_id = message.chat.id
-    email = message.text
-    if "@" not in email:
-        bot.send_message(chat_id, "بريد إلكتروني غير صالح. الرجاء إعادة المحاولة.")
-        return
-
-    user_data[chat_id]["mode"] = "s2"
-    user_data[chat_id]["to"] = email
-    bot.send_message(chat_id, "ارسل لي الموضوع الخاص بالرسالة:")
-
-@bot.message_handler(func=lambda message: user_data[message.chat.id]["mode"] == "s2")
-def send_email_step3(message):
-    chat_id = message.chat.id
-    subject = message.text
-
-    user_data[chat_id]["mode"] = "s3"
-    user_data[chat_id]["sub"] = subject
-    bot.send_message(chat_id, "ارسل النص الذي ترغب في إرساله:")
-
-@bot.message_handler(func=lambda message: user_data[message.chat.id]["mode"] == "s3")
-def send_email_step4(message):
-    chat_id = message.chat.id
-    text = message.text
-
-    user_data[chat_id]["mode"] = "s4"
-    user_data[chat_id]["msg"] = text
-    bot.send_message(chat_id, "كم مرة تريد إرسال هذه الرسالة:")
-
-@bot.message_handler(func=lambda message: user_data[message.chat.id]["mode"] == "s4")
-def send_email_step5(message):
-    chat_id = message.chat.id
-    count = message.text
-
-    if not count.isnumeric():
-        bot.send_message(chat_id, "برجاء إدخال رقم صحيح.")
-        return
-
-    user_data[chat_id]["mode"] = "s5"
-    user_data[chat_id]["msgs"] = count
-
-    email = user_data[chat_id]["to"]
-    subject = user_data[chat_id]["sub"]
-    text = user_data[chat_id]["msg"]
-    count = int(count)
-
-
-    send_email(chat_id, email, subject, text, count)
-
-def send_email(chat_id, email, subject, text, count):
-
-##اصنع حساب بداخل مايكروسوفت وخلي معلوماتك باسورد وايميل حطه بلفراغات
-    smtp_server = "smtp.office365.com"
-    smtp_port = 587  
-    smtp_username = "ايميلك"
-    smtp_password = "ياسوردك"
-
-
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-
-    for _ in range(count):
-        msg = MIMEMultipart()
-        msg['From'] = smtp_username
-        msg['To'] = email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(text, 'plain'))
-
-        server.sendmail(smtp_username, email, msg.as_string())
-
-    server.quit()
-
-    bot.send_message(chat_id, f"تم إرسال الرسالة إلى {email} {count} مرة.")
-
-    user_data[chat_id]["mode"] = None
-    show_keyboard(chat_id)
-
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+# تشغيل الدالة الزمنية بشكل دائم باستخدام asyncio
+if __name__ ==  __main__ :
+    loop = asyncio.get_event_loop()
+    loop.create_task(update_channel_description())
+    loop.run_forever()
